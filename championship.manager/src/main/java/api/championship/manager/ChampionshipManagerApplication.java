7ab1043ddda33174e71108acd49d84a68ca3ae63
Lucas Soares <soares0lucas8@gmail.com>
2022-08-31 14:39:50 -0300

    first commit

---
+++ championship.manager/src/main/java/api/championship/manager/ChampionshipManagerApplication.java
@@ -0,0 +1,13 @@
+package api.championship.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionshipManagerApplication.class, args);
+	}
+
+}
---
+++ championship.manager/src/test/java/api/championship/manager/ChampionshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.championship.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
