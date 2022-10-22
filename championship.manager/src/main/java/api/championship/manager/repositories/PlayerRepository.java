d5de67f71d6fd5895ebea975426a9e6616e5a8a9
Lucas Soares <soares0lucas8@gmail.com>
2022-10-22 13:32:27 -0300

    adicionando crud de jogador

---
+++ championship.manager/src/main/java/api/championship/manager/repositories/PlayerRepository.java
@@ -0,0 +1,9 @@
+package api.championship.manager.repositories;
+
+import api.championship.manager.models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
---
+++ championship.manager/src/main/java/api/championship/manager/services/PlayerService.java
@@ -0,0 +1,65 @@
+package api.championship.manager.services;
+
+import api.championship.manager.execeptionHandler.exceptions.MessageNotFoundException;
+import api.championship.manager.models.Player;
+import api.championship.manager.models.Team;
+import api.championship.manager.repositories.PlayerRepository;
+import api.championship.manager.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PlayerService {
+    @Autowired
+    private PlayerRepository playerRepository;
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public void addPlayer(Player newPlayer){
+        try {
+            Optional<Team> team = teamRepository.findById(newPlayer.getTeam().getId());
+            if (team.isEmpty())
+                throw new MessageNotFoundException("Time não existe");
+
+            Player player = new Player();
+            player.setName(newPlayer.getName());
+            player.setTeam(team.get());
+            playerRepository.save(player);
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+
+    public void updatePlayer(Player newPlayer){
+        try {
+            Optional<Team> team = teamRepository.findById(newPlayer.getTeam().getId());
+            if (team.isEmpty())
+                throw new MessageNotFoundException("Time não existe");
+
+            Optional<Player> player = playerRepository.findById(newPlayer.getId());
+            if (player.isEmpty())
+                throw new MessageNotFoundException("Jogador não existe");
+
+            player = Optional.of(new Player());
+            player.get().setName(newPlayer.getName());
+            player.get().setTeam(team.get());
+            playerRepository.save(player.get());
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+
+    public void deletePlayer(Long id){
+        try {
+            Optional<Player> player = playerRepository.findById(id);
+            if (player.isEmpty())
+                throw new MessageNotFoundException("Jogador não existe");
+
+            playerRepository.delete(player.get());
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+}
